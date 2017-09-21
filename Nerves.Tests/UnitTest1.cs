10d53ce16f8a23ec855893d62f74eb667293ad14
pasidu <pasidu@gmail.com>
2017-09-20 20:35:11 -0700

    initial check from VS

---
+++ Nerves.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nerves.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
